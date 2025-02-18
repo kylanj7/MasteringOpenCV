571d734c1f8f0ac53a303a39fde89243d0afa73e
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-02-18 04:15:36 -0800

    Create AutoSquareSpiral.py

---
+++ AutoSquareSpiral.py
@@ -0,0 +1,12 @@
+import time
+import pyautogui
+time.sleep(2.0)
+
+distance = 200
+while distance > 0:
+        pyautogui.drag(distance, 0, button='left', duration=0.5)   # move right
+        distance -= 50
+        pyautogui.drag(0, distance, button='left', duration=0.5)   # move down
+        pyautogui.drag(-distance, 0, button='left', duration=0.5)  # move left
+        distance -= 50
+        pyautogui.drag(0, -distance, button='left', duration=0.5)  # move up
